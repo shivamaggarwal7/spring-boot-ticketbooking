2ffbdd006b958f304fb8ee750b9d104bae37ad09
Shivam <shivam.aggarwal@yahoo.co.in>
2018-12-23 16:33:12 +0530

    Initial import

---
+++ src/main/java/com/ticket/application/TicketApplication.java
@@ -0,0 +1,14 @@
+package com.ticket.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketApplication.class, args);
+	}
+
+}
+
